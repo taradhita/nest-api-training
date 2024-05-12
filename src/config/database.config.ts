e96409f6c8b11c278e66592fd807851bd1bbd8bb
taradhita <taradhita@gmail.com>
2024-05-12 13:55:37 +0800

    set up prisma

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,4 +3,8 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('database', () => ({
   host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT || 5432,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_DB,
+  url: process.env.DATABASE_URL,
 }));
