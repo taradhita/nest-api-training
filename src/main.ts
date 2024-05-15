60f864209267c50e34cd507b5322986fb17e144e
taradhita <taradhita@gmail.com>
2024-05-15 16:34:10 +0800

    set validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { VersioningType } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +11,7 @@ async function bootstrap() {
     type: VersioningType.URI,
   });
   app.setGlobalPrefix('api/');
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(config.get('app.http.port'));
 }
 bootstrap();
