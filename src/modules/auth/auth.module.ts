29ae7f1831afe57e4539802ccdfe7ffc10b517a7
taradhita <taradhita@gmail.com>
2024-05-15 16:33:30 +0800

    add and apply custom http exception

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, Scope } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from '../../app/v1/auth/auth.controller';
 import { UserModule } from '../user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from '../../app/exceptions/http.exception';
 
 @Module({
   imports: [
@@ -14,7 +16,14 @@ import { JwtModule } from '@nestjs/jwt';
       signOptions: { expiresIn: '1d' },
     }),
   ],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    {
+      provide: APP_FILTER,
+      scope: Scope.REQUEST,
+      useClass: HttpExceptionFilter,
+    },
+  ],
   controllers: [AuthController],
   exports: [AuthService],
 })
