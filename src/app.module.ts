e1123c91267c67f7045d8406e60b5efb2336b720
taradhita <taradhita@gmail.com>
2024-05-15 13:11:36 +0800

    update app module and main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './modules/auth/auth.module';
 import databaseConfig from './config/database.config';
 import appConfig from './config/app.config';
+import { AuthController } from './app/v1/auth/auth.controller';
+import { UserModule } from './modules/user/user.module';
+import { PrismaModule } from './providers/database/prisma/prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      isGlobal: true,
       load: [appConfig, databaseConfig],
     }),
+    AuthModule,
+    UserModule,
+    PrismaModule,
   ],
-  controllers: [AppController],
+  controllers: [AppController, AuthController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  app.setGlobalPrefix('api/');
   await app.listen(config.get('app.http.port'));
 }
 bootstrap();
