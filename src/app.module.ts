d497561d3e1762f70039687b653490f32b7693e9
taradhita <taradhita@gmail.com>
2024-06-15 21:18:12 +0800

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
 import databaseConfig from './config/database.config';
 import appConfig from './config/app.config';
-import { AuthController } from './app/v1/auth/auth.controller';
 import { UserModule } from './modules/user/user.module';
 import { PrismaModule } from './providers/database/prisma/prisma.module';
 
@@ -19,7 +16,5 @@ import { PrismaModule } from './providers/database/prisma/prisma.module';
     UserModule,
     PrismaModule,
   ],
-  controllers: [AppController, AuthController],
-  providers: [AppService],
 })
 export class AppModule {}
