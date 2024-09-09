9cc5d15aa67abf2123247080b168218da4b2ae7e
taradhita <taradhita@gmail.com>
2024-09-09 15:27:04 +0800

    move APP_FILTER provider to http.v1 module

--- src/modules/category/category.module.ts
+++ src/modules/category/category.module.ts
@@ -1,18 +1,10 @@
-import { Module, Scope } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { PrismaModule } from '@/providers/database/prisma/prisma.module';
-import { APP_FILTER } from '@nestjs/core';
-import { HttpExceptionFilter } from 'src/app/http/exceptions/http.exception';
+
 @Module({
   imports: [PrismaModule],
-  providers: [
-    CategoryService,
-    {
-      provide: APP_FILTER,
-      scope: Scope.REQUEST,
-      useClass: HttpExceptionFilter,
-    },
-  ],
+  providers: [CategoryService],
   exports: [CategoryService],
 })
 export class CategoryModule {}
